35b29fb8a496f6f1b9209cda526a9c42d81cdb4e
MHorbunov <MHorbunov@MHorbunov.kiev.luxoft.com>
2019-07-12 17:19:44 +0300

    Create DAO

--- src/com/luxoft/log/dao/PersonDAO.java
+++ src/com/luxoft/log/dao/PersonDAO.java
@@ -6,12 +6,24 @@ import java.util.List;
 import com.luxoft.log.model.Person;
 
 public class PersonDAO {
+	private static PersonDAO instance = null;
+	
+	private PersonDAO() {
+	}
+
+	public static PersonDAO getInstance() {
+		if (instance == null) {
+			instance = new PersonDAO();
+		}
+		
+		return instance;
+	}
 	
 	public List<Person> getAll() {
 		
 		List<Person> persons = new ArrayList<>();
 		persons.add(new Person("Вася", "1", true));
-		persons.add(new Person("Петя", "1", true));
+		persons.add(new Person("Петя", "1", false));
 		persons.add(new Person("Толик", "2", true));
 		
 		return persons;
